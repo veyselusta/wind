0ab875b79c36ab38607835d7b5c57691f1722113
Veysel Usta <veyselusta7@gmail.com>
2020-11-21 20:21:28 +0300

    uptade statement

--- soundControl.js
+++ soundControl.js
@@ -4,17 +4,14 @@ module.exports = function soundControl() {
         audio.play();
         audio.volume = 0.5;
         status = 'low';
-        return;
 
   if (status == 'low')
         audio.volume = 1;
         status = 'high';
-        return;
-
+        
   if (status == 'high')
         audio.pause();
         status = 'none';
-        return;
   }
 
   chrome.browserAction.setIcon({
