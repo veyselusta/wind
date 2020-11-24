3dead2d4b34ea6708b081dcbfef900af34761ab6
Veysel Usta <37637870+veyselusta@users.noreply.github.com>
2020-11-24 14:47:50 +0300

    soundControl function statement became switch case

--- soundControl.js
+++ soundControl.js
@@ -1,24 +1,26 @@
-module.exports = function soundControl() {
-
-  if (status == 'none') {
-        audio.play();
-        audio.volume = 0.5;
-        status = 'low';
-
-  if (status == 'low')
-        audio.volume = 1;
-        status = 'high';
-        
-  if (status == 'high')
-        audio.pause();
-        status = 'none';
-  }
-
-  chrome.browserAction.setIcon({
-    path: {
-      "19": `assets/${status}19px.png`,
-      "38": `assets/${status}38px.png`
+module.exports =   function soundControl() {
+  
+    switch (status) {
+      case 'none':
+        audio.play()
+        audio.volume = 0.5
+        status = 'low'
+        break
+      case 'low':
+        audio.volume = 1
+        status = 'high'
+        break
+      case 'high':
+        audio.pause()
+        status = 'none'
+        break
     }
-  });
 
-}
+    chrome.browserAction.setIcon({
+      path: {
+        "19": "images/" + status + "19px.png",
+        "38": "images/" + status + "38px.png"
+      }
+    })
+
+  }
