f9fd65f9a41452c929d384c045e0123c13268846
Veysel Usta <37637870+veyselusta@users.noreply.github.com>
2020-11-27 15:44:09 +0300

    Update wind.js

--- wind.js
+++ wind.js
@@ -5,4 +5,32 @@ const audio = document.createElement('audio')
 audio.setAttribute('src', 'wind.mp3')
 audio.setAttribute('loop', 'true')
 
+   function soundControl() {
+  
+    switch (status) {
+      case 'none':
+        audio.play()
+        audio.volume = 0.5
+        status = 'low'
+        break
+      case 'low':
+        audio.volume = 1
+        status = 'high'
+        break
+      case 'high':
+        audio.pause()
+        status = 'none'
+        break
+    }
+
+    chrome.browserAction.setIcon({
+      path: {
+        "19": "images/" + status + "19px.png",
+        "38": "images/" + status + "38px.png"
+      }
+    })
+
+  }
+
+
 chrome.browserAction.onClicked.addListener(soundControl)
